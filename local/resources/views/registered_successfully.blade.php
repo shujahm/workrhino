3fc0835fb5e3b1f3636332165e9e140e1061cfb9
MehrozAnsari <mehrozansari7@gmail.com>
2019-09-18 22:25:37 +0500

    Create registered_successfully.blade.php
    i did add a page if user successfully registered then this page will show
    
    Co-Authored-By: mehrozansari <mehrozansari@users.noreply.github.com>

---
+++ local/resources/views/registered_successfully.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+   
+
+   @include('style')
+	
+</head>
+<body>
+
+   
+
+    <!-- fixed navigation bar -->
+    @include('header')
+
+    <!-- slider -->
+    
+		
+	
+	<div class="video">
+
+	<div class="headerbg">
+	 <div class="col-md-12" align="center"><h1>Yor are successfully registered</h1></div>
+	 </div>
+	<div class="">
+	 
+	 
+	 
+	</div>
+	</div>
+	   
+
+      @include('footer')
+</body>
+</html>
