d0d37e107b5e158c7a90678ac69abf6ad35d40ed
MehrozAnsari <mehrozansari7@gmail.com>
2019-09-19 12:10:15 +0500

    Update header.blade.php
    adding a new link
    
    Co-Authored-By: mehrozansari <mehrozansari@users.noreply.github.com>

--- local/resources/views/header.blade.php
+++ local/resources/views/header.blade.php
@@ -51,6 +51,7 @@ if(Auth::check())
             <li><a href="<?php echo $url;?>/register"><div style="color:#000000;">Sign Up</div></a></li>
             <li><a href="<?php echo $url;?>/login"><div style="color:#000000;">Login</div></a></li>
 			<li style="display:none"><a href="<?php echo $url;?>/new-request"  class="borbtn">Post a Job</a></li>
+			<li><a href="<?php echo $url;?>/register2"  class="borbtn">Become a Rhino</a></li>
             <?php } else { ?>
 			<li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown"> {{ Auth::user()->name }} <i class="fa fa-angle-down" aria-hidden="true"></i>
