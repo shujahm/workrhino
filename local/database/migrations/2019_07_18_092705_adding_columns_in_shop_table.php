3195460ae18e47dbafb34c9ea6b8ecbe9dbba149
MehrozAnsari <mehrozansari7@gmail.com>
2019-09-19 11:53:18 +0500

    Update 2019_07_18_092705_adding_columns_in_shop_table.php
    i did add a new migrarion for new column in shop table
    
    Co-Authored-By: mehrozansari <mehrozansari@users.noreply.github.com>

--- local/database/migrations/2019_07_18_092705_adding_columns_in_shop_table.php
+++ local/database/migrations/2019_07_18_092705_adding_columns_in_shop_table.php
@@ -16,13 +16,13 @@ class AddingColumnsInShopTable extends Migration
         Schema::table('shop', function (Blueprint $table) {
             $table->string('cnic');
             $table->string('gender');
-            $table->string('contact_no');
             $table->string('associated_with_contractor');
-            $table->string('name_of_firm');
+            $table->string('name_of_firm')->nullable();
             $table->date('date_of_registration');
-            $table->string('name_of_owner_or_contractor');
+            $table->string('name_of_owner_or_contractor')->nullable();
+            $table->string('profession');
            
-            $table->integer('number_of_rhinos_in_team');
+            $table->integer('number_of_rhinos_in_team')->nullable();
             $table->integer('shop_type_id');
             
         });
@@ -38,12 +38,12 @@ class AddingColumnsInShopTable extends Migration
         Schema::table('shop', function (Blueprint $table) {
             $table->dropColumn('cnic');
             $table->dropColumn('gender');
-            $table->dropColumn('contact_no');
             $table->dropColumn('associated_with_contractor');
             $table->dropColumn('name_of_firm');
             $table->dropColumn('date_of_registration');
             $table->dropColumn('name_of_owner_or_contractor');
-           
+            $table->dropColumn('profession');
+
             $table->dropColumn('number_of_rhinos_in_team');
             $table->dropColumn('shop_type_id');
             
